591ca30fde67ce100f2d048bf24bd867844c0826
Nomber86 <Licoshram@gmail.com>
2023-02-27 13:01:13 +0100

    Initial commit

---
+++ Test project git/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        LinkedList<int> numbers = new LinkedList<int>();
+        for (int i = 0; i < n; i++)
+        {
+            numbers.AddLast(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        var array1 = numbers.Where(number => number % 2 == 0)
+            .Select(number => number * 2);
+        
+        Console.WriteLine(string.Join(" " , array1));
+
+    }
+}
